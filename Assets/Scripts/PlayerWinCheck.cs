15980e239176fe9524abee2c48e45169d9d1dc80
The-Innominate <100097404+The-Innominate@users.noreply.github.com>
2025-02-24 09:28:26 -0700

    UI Update

--- Assets/Scripts/PlayerWinCheck.cs
+++ Assets/Scripts/PlayerWinCheck.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerWinCheck : MonoBehaviour
 {
+    //this will be UI elements that need to be disabled or enabled
+    public GameObject pauseMenuOverlay;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Winnable"))
@@ -10,6 +13,19 @@ public class PlayerWinCheck : MonoBehaviour
             if (GameObject.Find("GameEndMenu") != null)
             {
                 //get the player win panel (child of canvas), and display it
+                pauseMenuOverlay.SetActive(false);
+                GameObject.Find("GameEndMenu").transform.GetChild(1).gameObject.SetActive(true);
+
+            }
+        }
+
+        if (collision.gameObject.CompareTag("PlayerKiller"))
+        {
+            //check if a canvas with the name "GameEndMenu" exists
+            if (GameObject.Find("GameEndMenu") != null)
+            {
+                //get the player lose panel (child of canvas), and display it
+                pauseMenuOverlay.SetActive(false);
                 GameObject.Find("GameEndMenu").transform.GetChild(0).gameObject.SetActive(true);
             }
         }
