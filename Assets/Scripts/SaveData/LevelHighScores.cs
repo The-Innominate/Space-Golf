c750b485e78f9be8662a680df55eea0a01651b46
JacobChurch2 <jchurch@student.neumont.edu>
2025-08-19 14:00:24 -0600

    Encrypted Json

--- Assets/Scripts/SaveData/LevelHighScores.cs
+++ Assets/Scripts/SaveData/LevelHighScores.cs
@@ -1,68 +1,88 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using Newtonsoft.Json; // Ensure you have the Newtonsoft.Json package installed
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
 
 public sealed class LevelHighScores
 {
-    private static LevelHighScores instance;
+	private static LevelHighScores instance;
+	private string filePath = Application.persistentDataPath + "/levelHighScores.json";
+
+	// === Key Obfuscation (Split + Base64 + Scrambled + Decoy Junk) ===
+	private static string GetEncryptionKey()
+	{
+		// Base64 encoded parts (some are fake junk!)
+		string[] scrambledParts = new string[]
+		{
+			"QmFkUGFydDE=",        // "BadPart1" (junk)
+            "UzNjcjN0",            // "S3cr3t" (real, 2nd piece)
+            "U3VwM3I=",            // "Sup3r" (real, 1st piece)
+            "SmFuazEyMw==",        // "Jank123" (junk)
+            "SzN5MTIzNDU2Nzg5MCE=", // "K3y1234567890!" (real, 3rd piece)
+            "RmFrZUtleQ=="         // "FakeKey" (junk)
+        };
+
+		// The correct order to reassemble only the REAL ones
+		int[] indexMap = { 2, 1, 4 };
+
+		StringBuilder sb = new StringBuilder();
+		foreach (int i in indexMap)
+		{
+			string decoded = Encoding.UTF8.GetString(System.Convert.FromBase64String(scrambledParts[i]));
+			sb.Append(decoded);
+		}
+
+		string fullKey = sb.ToString(); // "Sup3rS3cr3tK3y1234567890!"
+		return fullKey;
+	}
+
+	// 16 bytes IV (AES block size)
+	private static readonly byte[] iv = new byte[16];
 
 	public static LevelHighScores Instance
 	{
 		get
 		{
 			if (instance == null)
-			{
 				instance = new LevelHighScores();
-			}
 			return instance;
 		}
 	}
 
 	private LevelHighScores()
 	{
-		// Constructor logic here
-
-		if(!File.Exists(filePath))
+		if (!File.Exists(filePath))
 		{
-			File.WriteAllText(filePath, "{}"); // Empty JSON object
+			SaveEncrypted("{}"); // write empty encrypted JSON
 			Debug.Log("File created at: " + filePath);
 		}
 	}
-
-	private string filePath = Application.persistentDataPath + "/levelHighScores.json";
 
 	public void SaveToJson(string course, string levelName, int score)
 	{
 		var data = LoadAllScores();
 		if (data == null)
-		{
 			data = new Dictionary<string, Dictionary<string, int>>();
-		}
 
 		if (!data.ContainsKey(course))
-		{
 			data[course] = new Dictionary<string, int>();
-		}
 
 		if (!data[course].ContainsKey(levelName))
-		{
 			data[course][levelName] = 0;
-		}
 
-		int existingScore;
-		existingScore = data[course][levelName];
+		int existingScore = data[course][levelName];
 		if (existingScore > score || existingScore == 0)
 		{
 			data[course][levelName] = score;
 			string json = JsonConvert.SerializeObject(data, Formatting.Indented);
-			File.WriteAllText(filePath, json);
-			Debug.Log(filePath);
+			SaveEncrypted(json);
+			Debug.Log("Score saved.");
 		}
 		else
 		{
 			Debug.Log("Score not saved, existing score is lower.");
-			Debug.Log(filePath);
 		}
 	}
 
@@ -71,8 +91,8 @@ public sealed class LevelHighScores
 		if (!File.Exists(filePath))
 			return new Dictionary<string, Dictionary<string, int>>();
 
-		string json = File.ReadAllText(filePath);
-		Dictionary<string, Dictionary<string, int>> data = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, int>>>(json);
+		string json = LoadDecrypted();
+		var data = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, int>>>(json);
 		if (data == null) data = new Dictionary<string, Dictionary<string, int>>();
 		return data;
 	}
@@ -80,74 +100,45 @@ public sealed class LevelHighScores
 	public int LoadFromJson(string course, string levelName)
 	{
 		var data = LoadAllScores();
-		if (!data.ContainsKey(course))
-			return 0;
-		if (!data[course].ContainsKey(levelName))
-			return 0;
+		if (!data.ContainsKey(course)) return 0;
+		if (!data[course].ContainsKey(levelName)) return 0;
 		return data[course][levelName];
 	}
 
-	//public void SaveToJson(string levelName, int Score)
-	//{
-	//	var theData = LoadFromJson();
-	//	string data;
-	//	int previousScore = LoadFromJson(levelName); // Load existing data to avoid overwriting
-	//	if (previousScore == 0)
-	//	{
-	//		theData.Add(levelName, Score.ToString()); // Add new level and score
-	//		data = JsonUtility.ToJson(theData, true);
-	//		System.IO.File.WriteAllText(filePath, data); // Save the updated data
-	//	}
-	//	else if(previousScore > Score)
-	//	{
-	//		theData[levelName] = Score.ToString();
-	//		data = JsonUtility.ToJson(theData, true);
-	//		System.IO.File.WriteAllText(filePath, data); // Save the updated data
-	//	}
-	//}
+	// ====================
+	// 🔒 Encryption Helpers
+	// ====================
+	private void SaveEncrypted(string plainText)
+	{
+		using (Aes aes = Aes.Create())
+		{
+			aes.Key = Encoding.UTF8.GetBytes(GetEncryptionKey().PadRight(32).Substring(0, 32));
+			aes.IV = iv;
 
-	//private Dictionary<string, string> LoadFromJson()
-	//{
-	//	// Load the JSON data from the file
-	//	string json = System.IO.File.ReadAllText(filePath);
-	//	// Deserialize the JSON data into a dictionary
-	//	var levelHighScores = JsonUtility.FromJson<Dictionary<string, string>>(json);
-	//	return levelHighScores;
-	//}
+			ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+			using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+			using (CryptoStream cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Write))
+			using (StreamWriter sw = new StreamWriter(cs))
+			{
+				sw.Write(plainText);
+			}
+		}
+	}
 
-	//public int LoadFromJson(string levelName)
-	//{
-	//	// Load the JSON data from the file
-	//	string json = System.IO.File.ReadAllText(filePath);
-	//	// Deserialize the JSON data into a dictionary
-	//	var levelHighScores = JsonUtility.FromJson<Dictionary<string, string>>(json);
+	private string LoadDecrypted()
+	{
+		using (Aes aes = Aes.Create())
+		{
+			aes.Key = Encoding.UTF8.GetBytes(GetEncryptionKey().PadRight(32).Substring(0, 32));
+			aes.IV = iv;
 
-	//	string highScore = "0"; // Default value if the level name does not exist
-	//							// Check if the level name exists in the dictionary
-	//	if (levelHighScores.ContainsKey(levelName))
-	//	{
-	//		// Get the high score for the level
-	//		highScore = levelHighScores[levelName];
-	//		Debug.Log("High Score for " + levelName + ": " + highScore);
-	//	}
-	//	else
-	//	{
-	//		Debug.Log("No high score found for " + levelName);
-	//		return 0; // Return 0 if the level name does not exist
-	//	}
-
-	//	// Return the high score as an integer
-
-	//	try
-	//	{
-	//		return int.Parse(highScore);
-	//	}
-	//	catch (System.Exception e)
-	//	{
-	//		Debug.LogError("Error parsing high score: " + e.Message);
-	//	}
-
-	//	// If the level name does not exist, return 0 or some default value
-	//	return 0;
-	//}
+			ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+			using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+			using (CryptoStream cs = new CryptoStream(fs, decryptor, CryptoStreamMode.Read))
+			using (StreamReader sr = new StreamReader(cs))
+			{
+				return sr.ReadToEnd();
+			}
+		}
+	}
 }
